dbadd113fe226e7c7fa40f068d58a75d8eca338d
Igor Antun <igorantunc@hotmail.com>
2016-04-04 22:12:01 -0300

    Super secret classified stuff

---
+++ js/main.js
@@ -0,0 +1,13 @@
+cheet('c o o k i e', function() {
+  document.getElementById('status').style.color = '#81C0C5';
+  document.getElementById('status').innerHTML = '● COOKIES COOKIES COOKIES';
+  document.getElementById('name').innerHTML = 'Cookie Monster';
+  document.getElementById('subtitle').innerHTML = 'professional cookie eater';
+  document.getElementById('photo').src = 'http://static1.1.sqspcdn.com/static/f/803157/23603024/1380498888557/cookie_monster_anxiety.jpg?token=eV7pj%2FiD%2BWdEjBsaUg2QYhEYeSk%3D';
+  document.getElementById('description').innerHTML = "<h3><span>Today I will live in the moment unless it's unpleasant in which case I will eat a cookie</h3></span>";
+
+  document.getElementById('work-text').innerHTML = "Who care if me eat carrot or collard greens? Me also like broccoli and lettuce and lima beans. Me still Cookie Monster. That not a sham.";
+  document.getElementById('translations-text').innerHTML = "Until now Mr. Cookie Monster refused to talk about the matter because his mouth was full, and it's not polite to talk with your mouth full.";
+  document.getElementById('experience-text').innerHTML = "Sometimes me think what is love, and then me think love is what last cookie is for. Me give up the last cookie for you.";
+  document.getElementById('skills-text').innerHTML = "Friend something better than chocolate ice cream. Maybe friend somebody you give up last cookie for.";
+});
